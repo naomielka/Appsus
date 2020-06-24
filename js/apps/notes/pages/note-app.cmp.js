ac6d066c9a2ee0f5f89b177266fcea7e9982d377
yashaba <yashabazovner@gmail.com>
2020-06-24 12:48:33 +0300

    second

---
+++ js/apps/notes/pages/note-app.cmp.js
@@ -0,0 +1,20 @@
+export default {
+    template: `
+        <main class="app-main car-app">
+            <!-- <router-link to="/">New Car</router-link> -->
+        </main>
+    `,
+    data() {
+        return {
+
+        }
+    },
+    computed: {},
+    created() {},
+    methods: {},
+
+
+
+    components: {}
+
+}
