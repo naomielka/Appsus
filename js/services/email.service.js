8ab4f4a895cd552df95a4bf3dafdf0f8d0378f9f
Naomi <naomi.elka@gmail.com>
2020-06-24 17:03:41 +0300

    email-preview added 17:03

--- js/services/email.service.js
+++ js/services/email.service.js
@@ -1,23 +1,38 @@
-import { utilsService } from './utils.service.js'
+import { utilsService as utilService } from './utils.service.js'
 
 export const emailService = {
     getEmails,
+    getEmailById
 }
 
-var testEmails = [
-    { id: null, from: 'jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
-    { id: null, from: 'jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
-    { id: null, from: 'jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
-    { id: null, from: 'jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
-    { id: null, from: 'jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
-    { id: null, from: 'jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
-    { id: null, from: 'jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
-    { id: null, from: 'jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
-    { id: null, from: 'jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
+var gEmails = [
+    { id: '12HJ5', from: 'Jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
+    { id: '14JKI', from: 'Jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
+    { id: '255FF', from: 'Jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
+    { id: '12MK0', from: 'Jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
+    { id: '689NJ', from: 'Jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
+    { id: '45JUK', from: 'Jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
+    { id: '5JKI5', from: 'Jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
+    { id: 'KL95J', from: 'Jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
+    { id: 'MK5IS', from: 'Jason', subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
 
 ]
 
 function getEmails() {
-    testEmails.forEach((email) => { if (!email.id) { email.id = utilsService.getRandomId() } })
-    return Promise.resolve(testEmails)
+    return Promise.resolve(gEmails)
+}
+
+function getEmailById(emailId) {
+    var emailList = [];
+    if (utilService.loadFromStorage('emails')) {
+        emailList = utilService.loadFromStorage('emails');
+    } else {
+        getEmails()
+            .then((emails) => {
+                emailList = emails;
+                utilService.saveToStorage('emails', emails);
+            })
+    }
+    var email = emailList.find(email => email.id === emailId)
+    return email;
 }
