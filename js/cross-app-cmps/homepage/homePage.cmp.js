4e748c08f64f428f07d8fb3f72c395615a4c48af
Naomi <naomi.elka@gmail.com>
2020-06-26 20:23:29 +0300

    homepage css more improvements

--- js/cross-app-cmps/homepage/homePage.cmp.js
+++ js/cross-app-cmps/homepage/homePage.cmp.js
@@ -4,15 +4,14 @@ import { homepageService } from '../../services/homepage.service.js'
 export default {
     template: `
     <section class="homepage flex col align-center">
-        <h2>Welcome to our AppSus!</h2>
+        <!-- <h2>Welcome to our AppSus!</h2> -->
         <main class="flex row">
         <ul class="clean-list">
             <li v-for="creator in creators">
                 <about :creator="creator"></about>
             </li>
         </ul>
-            <section class="flex col">
-            <h2>Go To</h2>
+            <section class="nav-homepage flex col">
             <div class="link-bc flex justify-center align-center">
                 <div class="bc-div flex justify-center align-center">
                     <router-link class="nav-button " to="/"> Homepage</router-link>
