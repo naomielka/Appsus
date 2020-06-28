2e20ed840c94158555b2b6889224a9668e413c32
Naomi <naomi.elka@gmail.com>
2020-06-28 19:54:12 +0300

    sidebar icon

--- js/apps/email/cmps/email-sidebar.cmp.js
+++ js/apps/email/cmps/email-sidebar.cmp.js
@@ -4,7 +4,7 @@ export default {
         <div class="sticky-bar flex col align-center ">
             <button class="action-button open" @click="openSidebar">=</button>
             <router-link to="/newEmail">Compose</router-link>
-            <button @click="filterInbox">Inbox</button>
+            <button @click="filterInbox"> <i class="fas fa-inbox"></i> Inbox</button>
             <button @click="filterByStar">Starred</button>
             <button @click="filterBySent">Sent</button>
             <button @click="filterByDeleted">Trash</button>
